1c9e922fba586f720b6f742e9e3035b9c011704e
Macbook Pro 15 <macbookpro@MacBook-Pro-Macbook.local>
2022-09-12 22:22:53 +0300

    Crosspoint of straight lines

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,47 @@
-﻿// Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы
-Console.WriteLine("Hello, World!");
+﻿// Найти точку пересечения двух прямых заданных уравнением 
+//y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы
+
+Console.WriteLine("Найти точку пересечения двух прямых ");
+
+double[,] coeff = new double[2, 2];
+double[] coord = new double[2];
+
+void InputCoefficients(){
+  for (int i = 0; i < coeff.GetLength(0); i++)
+  {
+    Console.Write($"Введите коэффициенты {i+1}-го уравнения (y = k * x + b):\n");
+    for (int j = 0; j < coeff.GetLength(1); j++)
+    {
+      if(j==0) Console.Write($"Введите коэффициент k: ");
+      else Console.Write($"Введите коэффициент b: ");
+      coeff[i,j] = int.Parse(Console.ReadLine()??"0");
+    }
+  }
+}
+
+double[] Solution(double[,] coeff)
+{
+  coord[0] = (coeff[1,1] - coeff[0,1]) / (coeff[0,0] - coeff[1,0]);
+  coord[1] = coord[0] * coeff[0,0] + coeff[0,1];
+  return coord;
+}
+
+void OutputResponse(double[,] coeff)
+{
+  if (coeff[0,0] == coeff[1,0] && coeff[0,1] == coeff[1,1]) 
+  {
+    Console.Write($"\nПрямые совпадают");
+  }
+  else if (coeff[0,0] == coeff[1,0] && coeff[0,1] != coeff[1,1]) 
+  {
+    Console.Write($"\nПрямые параллельны");
+  }
+  else 
+  {
+    Solution(coeff);
+    Console.Write($"\nТочка пересечения прямых: ({coord[0]}, {coord[1]})");
+  }
+}
+
+InputCoefficients();
+OutputResponse(coeff);
